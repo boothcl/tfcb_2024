2c324f3890f8ccea8e03850f2c121808f1eed147
Clare Booth <boothc@uchicago.edu>
2024-10-14 21:04:45 +0000

    Update answer to hw 2 problem 1

--- homeworks/homework02/problem1_script.sh
+++ homeworks/homework02/problem1_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "My username is" $USER > question01.txt
+echo "My username is" `whoami` > question01.txt
 echo "My home directory is" $HOME >> question01.txt
 echo "The contents of the tfcb_2024/lectures/lecture04/ directory are" >> question01.txt
 ls /workspaces/tfcb_2024-copy/lectures/lecture04/ >> question01.txt
